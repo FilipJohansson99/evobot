9e3682cfbd28672831e34d46b4f41bd627a4f528
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-09-08 18:44:52 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -8,9 +8,6 @@ module.exports = {
     const queue = message.client.queue.get(message.guild.id);
     if (!queue) return message.channel.send("There is no queue.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
-    
-    if (!args.length) return message.reply(`Usage: ${message.client.prefix}clear`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}clear`);
     queue = [];
     queue.textChannel.send(`${message.author} ❌ cleared the queue.`);
   }
