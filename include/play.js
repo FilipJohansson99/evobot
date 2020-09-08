dc3649463818e55354a8ea3c77567fdad9e32cf0
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-09-09 00:52:59 +0200

    Update play.js

--- include/play.js
+++ include/play.js
@@ -76,7 +76,6 @@ module.exports = {
       if(ampersandPosition != -1) {
         video_id = video_id.substring(0, ampersandPosition);
       };
-      var postedBy = `${message.author}`;
 
       const embeddedMessage = new Discord.MessageEmbed()
 	    .setColor('#0099ff')
@@ -85,7 +84,7 @@ module.exports = {
       .setDescription(`${message.author}`)
       .setThumbnail(`https://img.youtube.com/vi/${video_id}/0.jpg`)
       .setTimestamp()
-      .setFooter(postedBy);
+      .setFooter(`${user}`);
 
       var playingMessage = await queue.textChannel.send(embeddedMessage);
       await playingMessage.react("⏭");
