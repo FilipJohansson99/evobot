f3c109a1195897a99d3c81530ddcf967f924f211
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-09-10 02:43:34 +0200

    Update postplaylist.js

--- commands/postplaylist.js
+++ commands/postplaylist.js
@@ -67,7 +67,7 @@ module.exports = {
         return message.reply("Playlist not found :(").catch(console.error);
       }
     }
-    var video_id = window.location.search.split('list=')[1];
+    var video_id = url.split('list=')[1];
     var ampersandPosition = video_id.indexOf('&');
     if(ampersandPosition != -1) {
       video_id = video_id.substring(0, ampersandPosition);
