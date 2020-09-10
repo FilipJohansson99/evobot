2def5c5d8dda725073e64a89906ee1afa0b6133b
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-09-10 02:41:27 +0200

    Update postplaylist.js

--- commands/postplaylist.js
+++ commands/postplaylist.js
@@ -67,11 +67,12 @@ module.exports = {
         return message.reply("Playlist not found :(").catch(console.error);
       }
     }
-    var video_id = window.location.search.split('v=')[1];
+    var video_id = window.location.search.split('list=')[1];
     var ampersandPosition = video_id.indexOf('&');
     if(ampersandPosition != -1) {
       video_id = video_id.substring(0, ampersandPosition);
     }
+
       let playlistEmbed = new MessageEmbed()
       .setTitle(`${playlist.title}`)
       .setURL(playlist.url)
