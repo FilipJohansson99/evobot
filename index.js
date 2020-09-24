dbb5c155b82bd0efaeba022e6b90286ebcb56e18
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-09-24 15:33:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 client.on('message', message => {
 if (message.channel.id === '384501410210906113') {
-  message.delete(43200000);
   if (message.author.id != '752358465682407475') {
     message.delete();
   }
