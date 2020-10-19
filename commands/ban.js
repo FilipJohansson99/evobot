f77b2d9e01c1315e711373f376238c86af0a8a06
Filip Johansson <51074752+FilipJohansson99@users.noreply.github.com>
2020-10-19 15:44:35 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -24,7 +24,7 @@ module.exports = {
       .setURL('steam://rungameid/231430')
       .setAuthor(Author)
       .setDescription('Klicka på länken för att varifiera ban.')
-      .setThumbnail(message.mention.user.displayAvatarURL({ format: 'png', dynamic: true }))
+      .setThumbnail(message.mentions.users.displayAvatarURL({ format: 'png', dynamic: true }))
       .setTimestamp()
       .setFooter(`${message.author.tag}`,message.author.displayAvatarURL({ format: 'png', dynamic: true }));
         message.channel.send(exampleEmbed);
